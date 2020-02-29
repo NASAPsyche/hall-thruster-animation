edf0d98de529dd2807cfe3e44f4d0d6e9ad19b9b
Shrey Arora <shreyarora2198@gmail.com>
2020-02-29 13:02:59 -0500

    electrons movement first draft from cathodes

--- Draft1_Animation/Assets/Scripts/Oscillator.cs
+++ Draft1_Animation/Assets/Scripts/Oscillator.cs
@@ -9,6 +9,7 @@ public class Oscillator : MonoBehaviour
     public float timeCounter = 0;
     public bool beginAnimation = false;
     private Animation anim;
+
     // Start is called before the first frame update
     public void Start()
     {
@@ -20,18 +21,24 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        //Debug.Log("Update" + beginAnimation);
-        // Check for space input and set boolean to opposite
       // if(Input.GetKeyDown(KeyCode.E)){
       //   beginAnimation = !beginAnimation;
       // }
-        //only change electrons when begin animation is true
     }
 
     public void SetActiveTrue(){
       this.gameObject.SetActive(true);
       anim = this.gameObject.GetComponent<Animation>();
-      anim.Play("e1cathode");
+      string electronName = this.gameObject.name;
+      char electronNumAnimation = ' ';
+      // getting the number of electron to play the animation
+      for(int i = electronName.Length - 1; i>=0; i--)
+      {
+        if(electronName[i] >= 48 && electronName[i] <= 57){
+          electronNumAnimation = electronName[i];
+        }
+      }
+      anim.Play("e" + electronNumAnimation + "cathode");
       StartCoroutine("DisableScript");
 
     }
@@ -46,6 +53,10 @@ public class Oscillator : MonoBehaviour
       if(beginAnimation)
       {
          transform.position = MoveElectron();
+         // Debug.Log(transform.position.x);
+         // Debug.Log(transform.position.y);
+         // Debug.Log(transform.position.z);
+         // beginAnimation = false;
       }
     }
 
@@ -54,16 +65,14 @@ public class Oscillator : MonoBehaviour
       return timeCounter;
     }
 
-
+    // oscillating electrons around the chamber
     public Vector3 MoveElectron(){
         timeCounter += Time.deltaTime;
         float R = 1/1.7f + Mathf.Cos(5*timeCounter)/17f;
         float Rz = Mathf.Cos(timeCounter/8)/17f;
-        //timeCounter += transform.position.x;
         float x = R*Mathf.Cos(timeCounter*2);
         float z = R*Mathf.Sin(timeCounter*2);
         float y=0;
-        //float y = Rz*Mathf.Cos(5*timeCounter)/17f;
         return new Vector3 (x,y,z);
     }
 
