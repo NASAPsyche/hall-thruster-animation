3f7dc1746998af42d77e814c45e0aae261b4ed8f
Shrey Arora <shreyarora2198@gmail.com>
2020-05-07 16:34:24 -0400

    Building

--- ci/docker_build.sh
+++ ci/docker_build.sh
@@ -2,14 +2,13 @@
 
 set -e
 echo hello
-pwd
 docker run \
   -e BUILD_NAME \
   -e UNITY_LICENSE_CONTENT \
   -e BUILD_TARGET \
   -e UNITY_USERNAME \
   -e UNITY_PASSWORD \
-  -w /project/ \
-  -v $(pwd):/project/ \
+  -w /hall-thruster-animation/ \
+  -v $(pwd):/hall-thruster-animation/ \
   $IMAGE_NAME \
-  /bin/bash -c "/project/ci/before_script.sh && /project/ci/build.sh"
+  /bin/bash -c "/hall-thruster-animation/ci/before_script.sh && /hall-thruster-animation/ci/build.sh"
